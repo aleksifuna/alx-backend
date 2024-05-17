ef9d72c373492947afd5f922e2cdfd3c59059284
Alex Sifuna <aleksifuna@gmail.com>
2024-05-17 23:28:28 +0300

    made default timezone uppercase

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,7 +3,7 @@
 Defines a Flask APP
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
@@ -13,7 +13,7 @@ class Config:
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'utc'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
