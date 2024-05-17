1c0d9db07b865d4a4e1cb96eb2120fa1502cf04e
Alex Sifuna <aleksifuna@gmail.com>
2024-05-17 23:36:34 +0300

    made some changes to the app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -13,7 +13,7 @@ class Config:
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'utc'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
