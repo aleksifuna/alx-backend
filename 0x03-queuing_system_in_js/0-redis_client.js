cf287fc590d38e3d3fc43d580b773db4a6628264
Alex Sifuna <aleksifuna@gmail.com>
2024-06-11 01:53:55 +0300

    added a script that sets and gets a value

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,12 +1,5 @@
 import { createClient } from 'redis';
 
-async function connectServer() {
-  const client = createClient();
-  client.on('error', (err) => {
-    console.log('Redis client not connected to the server:', err);
-  });
-  console.log('Redis client connected to the server');
-}
-connectServer().catch((err) => {
-  console.error(err);
-});
+const client = createClient();
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
